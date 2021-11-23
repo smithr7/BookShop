3696c7c1a60bacaad94709dac00f14734740b5d9
Ryan <smithr76@uni.coventry.ac.uk>
2021-11-23 17:21:28 +0000

    init binding

--- 5001CEM_BookShop_Project/BookShop/__init__.py
+++ 5001CEM_BookShop_Project/BookShop/__init__.py
@@ -7,13 +7,7 @@ def create_app():
     app.config['SECRET_KEY'] = '625273'
     
     with app.app_context():
-        from .loginCredentials import loginCredentials
-        from .loginCredentials import registerCredentials
-        from .indexScript import indexScript
-        from .booksCatalog import bookCatalog
+        from .loginCredentials.loginCredentials import loginCredentials
         
-        app.register_blueprint(loginCredentials.loginCredentials,url_prefix="/login")
-        app.register_blueprint(loginCredentials.registerCredentials)
-        app.register_blueprint(indexScript.indexScript,url_prefix="/index")
-        app.register_blueprint(booksCatalog.booksCatalog)
+        app.register_blueprint(loginCredentials,url_prefix="/login")
         
