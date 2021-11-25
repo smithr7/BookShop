9e78c8a198a3a7c13b9078c1e84b20ad34916852
Ryan <smithr76@uni.coventry.ac.uk>
2021-11-25 11:30:46 +0000

    bookInformation update

--- 5001CEM_BookShop_Project/BookShop/bookInformation/bookInformation.py
+++ 5001CEM_BookShop_Project/BookShop/bookInformation/bookInformation.py
@@ -6,13 +6,19 @@ from flask import request
 from flask import redirect
 from flask import abort
 from flask import make_response
+from flask import Blueprint
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 from re import *
 import sys,os
 
-app = Flask(__name__, template_folder='templates')
-app.secret_key = "625273"
+# bookInformation = Blueprint("bookInformation",__name__,
+#                             template_folder='templates')
+# bookInformation.secret_key = "625273"
+
+#Testing purposes
+
+app = Flask(__name__)
 
 @app.route('/bookDescription', methods=['POST','GET'])
 def bookDescription():
@@ -21,4 +27,12 @@ def bookDescription():
     cur = con.cursor()
     cur.execute("SELECT * FROM products WHERE ISBN13=?",(isbn,))
     record = cur.fetchone()
-    return render_template('bookPreview.html',product=isbn)
+    return render_template('bookPreview.html',Product=record)
+
+# @bookInformation.route('/bookDescription/<string:ISBN>', methods=['POST','GET'])
+# def bookDescription(ISBN):
+#     con = sqlite3.connect('../Database/bookProducts.db')
+#     cur = con.cursor()
+#     cur.execute("SELECT * FROM products WHERE ISBN13=?",(ISBN,))
+#     record = cur.fetchone()
+#     return render_template('bookPreview.html',product=isbn)
