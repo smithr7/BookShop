0917375c95f1820ea4777c7169ea12f9e7264fc2
Ryan <smithr76@uni.coventry.ac.uk>
2021-11-25 10:57:14 +0000

    Linking to init file

--- 5001CEM_BookShop_Project/BookShop/__init__.py
+++ 5001CEM_BookShop_Project/BookShop/__init__.py
@@ -8,8 +8,16 @@ def create_app():
     
     with app.app_context():
         from .loginCredentials.loginCredentials import loginCredentials
+        from .registerCredentials.registerCredentials import registerCredentials
+        from .indexScript.indexScript import indexScript
+        from .bookInformation.bookInformation import bookInformation
+        from .booksCatalog.booksCatalog import booksCatalog
         
         app.register_blueprint(loginCredentials,url_prefix="/login")
+        app.register_blueprint(registerCredentials)
+        app.register_blueprint(indexScript)
+        app.register_blueprint(bookInformation)
+        app.register_blueprint(booksCatalog)
         
         return app
         
--- 5001CEM_BookShop_Project/BookShop/bookInformation/bookInformation.py
+++ 5001CEM_BookShop_Project/BookShop/bookInformation/bookInformation.py
@@ -1,18 +1,24 @@
-from markupsafe import escape
-from flask import Flask, session, url_for, render_template, request, redirect, abort, make_response
+from flask import Flask
+from flask import session
+from flask import url_for
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import abort
+from flask import make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 from re import *
 import sys,os
 
-#Flask object instantiation of class with webpage directory path specified
-app = Flask(__name__, template_folder='../web/templates/')
+app = Flask(__name__, template_folder='templates')
 app.secret_key = "625273"
 
-@app.route('/bookDescription/<string:isbn>', methods=['POST'])
-def bookDescription(isbn):
-    con = sqlite3.connect('./Database/bookProducts.db')
+@app.route('/bookDescription', methods=['POST','GET'])
+def bookDescription():
+    isbn = "9780330520331"
+    con = sqlite3.connect('../Database/bookProducts.db')
     cur = con.cursor()
-    cur.execute("SELECT * FROM products WHERE ISBN13=?",isbn)
+    cur.execute("SELECT * FROM products WHERE ISBN13=?",(isbn,))
     record = cur.fetchone()
     return render_template('bookPreview.html',product=isbn)
