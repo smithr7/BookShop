ddbcad7f5b74a2ffe91a18766aa08a0196e6b115
Ryan <smithr76@uni.coventry.ac.uk>
2021-11-27 20:55:20 +0000

    Administration page

--- 5001CEM_BookShop_Project/BookShop/administration/administration.py
+++ 5001CEM_BookShop_Project/BookShop/administration/administration.py
@@ -7,23 +7,36 @@ from flask import current_app as app
 from markupsafe import escape
 import sqlite3, os, re
 
-administration = Blueprint('administration',__name__,
-                          template_folder="templates",
-                          static_folder="static")
+app = Flask(__name__,template_folder="templates",static_folder="static")
 
-@administration.route('/admin_dashboard', methods=['GET'])
+@app.route('/admin_dashboard', methods=['GET','POST'])
 def adminDashboard():
-    dbConnection = sqlite.connect('./Database/bookProducts.db')
+    dbConnection = sqlite3.connect('../Database/bookProducts.db')
     cursor = dbConnection.cursor()
     cursor.execute("SELECT * FROM products")
     rows = cursor.fetchall()
     return render_template("administration.html",Products=rows)
 
-@administration.route('/delete_record', methods=['POST','GET'])
+@app.route('/delete_record', methods=['POST','GET'])
 def delete_record(ISBN):
     dbConnection = sqlite.connect('./Database/bookProducts.db')
     cursor = dbConnection.cursor()
     cursor.execute("DELETE FROM products WHERE ISBN13=?",(ISBN,))
+
+@app.route('/edit_item/<string:ISBN>', methods=['POST','GET'])
+def edit(ISBN):
+    dbConnection = sqlite.connect('./Database/bookProducts.db')
+    cursor = dbConnection.cursor()
+    newQuantity = request.form['quantity']
+    cursor.execute("UPDATE products SET quantity =? WHERE ISBN13 =?",(newQuantity,ISBN))
+    return render_template("administration.html")
+# <!--{% assets "indexScript_style_bundle" %}
+#         <link 
+#          rel="stylesheet" 
+#          href="{{ ASSET_URL }}"
+#          type="text/css"
+#         />
+#         {% endassets %}-->
     
     
     
